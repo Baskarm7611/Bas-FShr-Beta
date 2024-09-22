9746061c37087c65c001250b14dbf899f2b1cf2b
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2024-09-22 22:06:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,10 +41,10 @@ if DB_URL:
     env_vars = {env['var_name']: env['value'] for env in col.find()}
     for key, value in env_vars.items():
         current_value = os.getenv(key)
-        if current_value is None or current_value == str(value):
+        if value:
             os.environ[key] = str(value)
         else:
-            logging.info(f"Skipping update for {key}, keeping the original value.")
+            logging.info(f"No Value Found In Db For {key} so use default")
     conn.close()
 
 # Other environment variables and settings
