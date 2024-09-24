c0fdb4d1bc52923543a684d1d84c93cf572d994c
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2024-09-24 17:32:24 +0530

    Update token_verfiy.py

--- database/token_verfiy.py
+++ database/token_verfiy.py
@@ -66,7 +66,7 @@ async def check_user_access(client, message):
         new_token = generate_token()
         await db.update_user(user_id, {'token': new_token})
         verification_link = await convert_short_link(f"https://telegram.me/{client.username}?start=VERIFY-{new_token}")
-        await message.reply_text(f"Your Ads Token is expired, refresh your token and try again. \n\nToken Timeout: {format_duration(TOKEN_VERIFY_TIME)}\n\nVerify Link : {verification_link}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Verify Link", url=verification_link)], [InlineKeyboardButton("How To Verify", url=TUTORIAL_VIDEO)]]))
+        await message.reply_text(f"Your Ads Token is expired, refresh your token and try again. \n\nToken Timeout: {format_duration(TOKEN_VERIFY_TIME)}\n\nVerify Link : {verification_link}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Verify Link", url=verification_link)], [InlineKeyboardButton("How To Verify", url=TUTORIAL_VIDEO)]]), protect_content=CONFIG_DICT['PROTECT_CONTENT'])
         return False
 
     return True
