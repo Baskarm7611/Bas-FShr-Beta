1fe7b7ab2d5fbc6ff38b45a5f53a65e75fee5c83
Baskarm7611 <64820629+Baskarm7611@users.noreply.github.com>
2024-10-16 10:17:07 +0530

    Update token_verfiy.py

--- database/token_verfiy.py
+++ database/token_verfiy.py
@@ -66,7 +66,7 @@ async def check_user_access(client, message):
         new_token = generate_token()
         await db.update_user(user_id, {'token': new_token})
         verification_link = await convert_short_link(f"https://telegram.me/{client.username}?start=VERIFY-{new_token}", is_token=True)
-        await message.reply_text(f"Your Ads Token is expired, refresh your token and try again. \n\nToken Timeout: {format_duration(TOKEN_VERIFY_TIME)}\n\nVerify Link : {verification_link}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Verify Link", url=verification_link)], [InlineKeyboardButton("How To Verify", url=TUTORIAL_VIDEO)]]), protect_content=True)
+        await message.reply_text(f"Your Verify Link is expired, refreshed your Verify Token and try again. \n\nToken Timeout: {format_duration(TOKEN_VERIFY_TIME)}\n\nHow It's Working ?\n\nIf you Verify 1 Time, Get Unlimited for Next {format_duration(TOKEN_VERIFY_TIME)}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Verify Link", url=verification_link)], [InlineKeyboardButton("How To Verify", url=TUTORIAL_VIDEO)]]), protect_content=True)
         return False
 
     return True
